5dbe8b68f1041f2c675eb936b3dfcbe9f29eaea8
sagar sahu <ch12b069@gmail.com>
2020-10-06 13:02:17 +0530

    updated code

---
+++ while balance.py
@@ -0,0 +1,15 @@
+
+i = 800.34
+n= int(input('press 1 to check the balance and 0 to exit'))
+if n!=1:
+    print('you can exit now')
+while n==1:
+          a = float(input('enter the transaction amount = '))
+          t = i+a
+          print( 'your account balance after this transaction is ',round(t,2))
+
+
+
+
+        
+    
