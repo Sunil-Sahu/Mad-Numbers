be6b40d8f92f6e270918b1f4cca484867a97119d
sagar sahu <ch12b069@gmail.com>
2020-10-06 13:06:01 +0530

    latest

---
+++ test.py
@@ -0,0 +1,7 @@
+x= int(input("enter first value : "))
+y= int(input("enter second value : "))
+z= int(input("enter third value : "))
+p = max(x,y,z)
+print (" Maximum of above values is :",p)
+
+input(" \n please press enter to exit ")
